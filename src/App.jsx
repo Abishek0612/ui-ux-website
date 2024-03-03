e70572a4e0b0bda2aaa35bd27e6713fc84da047a
Abishek0612 <uabishek6@gmail.com>
2024-03-04 00:49:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-white flex underline justify-center items-center font-bold bg-blue-600">
+        Hello Vite
+      </h1>
+    </>
+  );
+}
+
+export default App;
